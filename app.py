6983379fc8ac28a52e7ebd8ef71675f7796cd01b
Sunny Pepple <slpepple01@gmail.com>
2024-07-03 14:31:40 -0500

    updated ip base

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import json
 from dotenv import dotenv_values
 
 app = Flask(__name__)
-
 API_KEY = dotenv_values(".env") ['IP_BASE_API_KEY']
 
 @app.route("/")
@@ -17,10 +16,11 @@ def hello_world():
     try:
         if request.method=="GET":
             visitor_name = request.args.get('visitor_name')
-            config = dotenv_values(".env")
-            # location_namer = request.headers.get('X-Forwarded-For', request.remote_addr)
-            ip_address=request.environ['REMOTE_ADDR']
-            temperature =0
+            print("API_KEY",API_KEY)
+            
+            # location_namer = 
+            ip_address= request.environ['REMOTE_ADDR']
+            temperature=0
             city=""
 
             url= f'https://api.ipbase.com/v1/json/{ip_address}'
@@ -28,17 +28,21 @@ def hello_world():
 
             if response.status_code == 200:
                 data = response.json()
+                print("DATA",data)
 
                 city = data['city']
                 temperature = get_temperature(data.get('latitude'),data.get('longitude'), API_KEY)
 
-            result = {
-                "client_ip": request.environ['REMOTE_ADDR'],
-                "location":city,
-                "greeting":f"Hello,{visitor_name}!, the temperature is {temperature} degrees Celsius in {city}"
-            }
+                result = {
+                    "client_ip": ip_address,
+                    "location":city,
+                    "greeting":f"Hello,{visitor_name}!, the temperature is {temperature} degrees Celsius in {city}"
+                }
+                return jsonify(result)
+            else:
+                return jsonify("The api failed to fetch")
 
-            return jsonify(result)
+
     except ConnectionError:
         print(ConnectionError)
     except EnvironmentError:
